6ba62b2eecf94512cdb81c84b6e47ded20a19c39
Prashant <prashantchaubey9795@gmail.com>
2020-11-15 16:01:23 +0000

    Implemented a basic raw level

---
+++ Assets/GameFiles/Scripts/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace GameFiles.Scripts
+{
+    public class Rocket : MonoBehaviour
+    {
+        public float LaunchThurst = 10;
+
+        private Rigidbody2D _rigidbody;
+        private bool _launched;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.bodyType = RigidbodyType2D.Static;
+        }
+
+        private void Update()
+        {
+            if (!_launched && Input.GetKeyDown(KeyCode.Space))
+            {
+                _launched = true;
+                _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+                _rigidbody.AddForce(transform.up * LaunchThurst);
+            }
+        }
+    }
+}
---
+++ Assets/GameFiles/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GameFiles.Scripts
+{
+    public class Rotator : MonoBehaviour
+    {
+        public float TurnSenstiivity = 1;
+        public float MaxSteerAngle = 30;
+        public Transform LaunchPlatformStand;
+
+        private float _horizontalInput;
+
+        private void Update()
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            var steerAngle = _horizontalInput * MaxSteerAngle * TurnSenstiivity;
+            transform.RotateAround(LaunchPlatformStand.transform.position, Vector3.back, steerAngle);
+        }
+    }
+}
