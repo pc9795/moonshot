229a53c00e87ca8b881207fe22ad0e26c3af8634
Prashant <prashantchaubey9795@gmail.com>
2020-11-25 02:31:10 +0000

    Implemented game over and game completion screens

--- Assets/GameFiles/Scripts/gui/GameCompletionScreen.cs
+++ Assets/GameFiles/Scripts/gui/GameCompletionScreen.cs
@@ -1,8 +1,28 @@
-﻿using UnityEngine;
+﻿using GameFiles.Scripts.managers;
+using GameFiles.Scripts.plain.objects;
+using UnityEngine;
 
 namespace GameFiles.Scripts.gui
 {
     public class GameCompletionScreen : MonoBehaviour
     {
+        private GuiManager _guiManager;
+
+        private void Start()
+        {
+            _guiManager = FindObjectOfType<GuiManager>();
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void MainMenu()
+        {
+            _guiManager.NavigateTo(GuiScreen.MainMenu);
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void Quit()
+        {
+            _guiManager.NavigateTo(GuiScreen.QuitConfirm);
+        }
     }
 }
--- Assets/GameFiles/Scripts/gui/GameOverScreen.cs
+++ Assets/GameFiles/Scripts/gui/GameOverScreen.cs
@@ -1,8 +1,35 @@
-﻿using UnityEngine;
+﻿using GameFiles.Scripts.managers;
+using GameFiles.Scripts.plain.objects;
+using UnityEngine;
 
 namespace GameFiles.Scripts.gui
 {
     public class GameOverScreen : MonoBehaviour
     {
+        private GuiManager _guiManager;
+
+        private void Start()
+        {
+            _guiManager = FindObjectOfType<GuiManager>();
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void Restart()
+        {
+            _guiManager.NavigateTo(GuiScreen.InGame);
+            GameManager.RestartCurrLevel();
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void MainMenu()
+        {
+            _guiManager.NavigateTo(GuiScreen.MainMenu);
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void Quit()
+        {
+            _guiManager.NavigateTo(GuiScreen.QuitConfirm);
+        }
     }
 }
